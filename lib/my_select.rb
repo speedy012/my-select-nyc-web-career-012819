1062eb1e5d50a6adb39e20bdde26ae030dc8caa8
Herman <hrivas5726@aol.com>
2019-01-21 20:41:25 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,5 +1,12 @@
-def my_select(collection)
- # your code here!
- collection.select 
+def my_select(array) 
+count = 0
+result = []
+
+while count < array.size
+  if yield(array[count]) == true
+  result.push(array[count])
 end
+  count+=1
+end
+result
 end
